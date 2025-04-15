6720816637a4e2ba6d0bbc1d47394f2c333346d7
Chen Cecilia Liu <liu@ukp.informatik.tu-darmstadt.de>
2025-04-15 09:34:33 +0200

    code update

--- src/utils/llm_query.py
+++ src/utils/llm_query.py
@@ -1,10 +1,22 @@
-import openai 
+import openai
 from openai import OpenAI
 from config import OPENAI_API_KEY, GROQ_API_KEY,HF_TOKEN
-from groq import Groq
-import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from huggingface_hub import login
+from transformers import BitsAndBytesConfig
+
+double_quant_config = BitsAndBytesConfig(
+   load_in_4bit=True,
+   bnb_4bit_use_double_quant=True,
+)
+import os
+
+model_name = os.environ['LLAMA_PATH']
+login(HF_TOKEN)
+
+llama_model = AutoModelForCausalLM.from_pretrained(model_name, device_map='auto', local_files_only=True, quantization_config=double_quant_config, low_cpu_mem_usage=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name, local_files_only=True)
 
 
 def query_gpt(prompt,engine='gpt-4o',temp=1):
@@ -16,28 +28,16 @@ def query_gpt(prompt,engine='gpt-4o',temp=1):
         return query_gpt(prompt,engine,temp)
     return response.choices[0].message.content.strip()
 
-
-def query_llama(prompt,model='meta-llama/Llama-3.3-70B-Instruct',temp=1):
-    login(HF_TOKEN)
-    pipeline=transformers.pipeline('text-generation',model=model,
-                                   model_kwargs={"torch_dtype":torch.bfloat16},
-                                   device_map="auto"
-                                   )
-    outputs=pipeline(prompt,temperature=temp)
-    response=outputs[0]["generated_text"][-1]
+@torch.no_grad()
+def query_llama(prompt, model='meta-llama/Llama-3.3-70B-Instruct',temp=1):
+    # Generate predictions
+    prompt = tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)
+    inputs = tokenizer.encode(prompt, return_tensors="pt").to("cuda") 
+    output = llama_model.generate(inputs, max_new_tokens=6000, temperature=1)
+    response = tokenizer.decode(output[0].tolist())
+    response = response.split("<|end_header_id|>")[-1].replace("<|eot_id|>", "").strip()
+    if not response.endswith("]"):
+        response = response + "]"
     return response 
 
-#def query_groq_llm(prompt,model='llama-3.3-70b-versatile',temp=1, top_p=1):
-  #  client = Groq(api_key=GROQ_API_KEY)
-  #  try:
-   #     response=client.chat.completions.create(messages=prompt,
-  #          model=model,
-   #         temperature=temp,
-  #          top_p=top_p,
-  #      )
-  #  except:
- #       return query_groq_llm(prompt,model,temp,top_p)
- #   return response.choices[0].message.content.strip()
 
-
-    
